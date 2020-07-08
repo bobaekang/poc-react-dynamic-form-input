464f481be43e9e330cd8d8ba7ce3d8dfd532ff54
bobaekang <bkang.dev@gmail.com>
2020-07-08 10:12:20 -0500

    Initial setup for generated form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
 import React from 'react'
+import Form from './components/Form'
+
+const config = {
+  checkbox: {
+    type: 'checkbox',
+    label: 'Simple checkbox',
+  },
+  number: {
+    type: 'number',
+    label: 'Simple number',
+    props: {
+      min: 0,
+      max: 10,
+    },
+  },
+  text: {
+    type: 'text',
+    label: 'Simple text',
+    props: {
+      pattern: '^[a-zA-Z]*$',
+    },
+  },
+  select: {
+    type: 'select',
+    label: 'Simple select',
+    options: ['foo', 'bar'],
+    props: {
+      placeholder: 'Select one',
+    },
+  },
+}
+
+const initialValues = {
+  checkbox: false,
+  number: 0,
+  text: '',
+  select: '',
+}
 
 function App() {
-  return <div>Hello World</div>
+  return (
+    <div style={{ display: 'flex' }}>
+      <div style={{ margin: '0 auto' }}>
+        <h1>Generated form</h1>
+        <Form config={config} initialValues={initialValues} />
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Form.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import Checkbox from './Checkbox'
+import Select from './Select'
+import TextField from './TextField'
+
+type InputConfig = {
+  type: string
+  label: string
+  options?: string[]
+  props?: {
+    [key: string]: any
+  }
+}
+
+type InputFactoryProps = {
+  name: string
+  config: InputConfig
+  value: any
+  onChange(event: any): void
+}
+
+const InputFactory = ({ name, config, value, onChange }: InputFactoryProps) => {
+  const { type, label, options, props } = config
+  switch (type) {
+    case 'text':
+    case 'number':
+      return (
+        <TextField
+          type={type}
+          name={name}
+          label={label}
+          value={value}
+          onChange={onChange}
+          {...props}
+        />
+      )
+    case 'checkbox':
+      return (
+        <Checkbox
+          name={name}
+          label={label}
+          checked={value}
+          onChange={onChange}
+          {...props}
+        />
+      )
+    case 'select':
+      return (
+        <Select
+          name={name}
+          label={label}
+          options={options ? options : []}
+          value={value}
+          onChange={onChange}
+          {...props}
+        />
+      )
+    default:
+      return null
+  }
+}
+
+type FormProps = {
+  config: {
+    [key: string]: InputConfig
+  }
+  initialValues: {
+    [key: string]: any
+  }
+}
+
+const Form = ({ config, initialValues }: FormProps) => {
+  const formik = useFormik({
+    initialValues: { ...initialValues },
+    onSubmit() {},
+  })
+
+  return (
+    <form>
+      {Object.keys(config).map((key) => (
+        <div style={{ margin: '1rem' }} key={key}>
+          <InputFactory
+            name={key}
+            config={config[key]}
+            value={formik.values[key]}
+            onChange={formik.handleChange}
+          />
+        </div>
+      ))}
+    </form>
+  )
+}
+
+export default Form
