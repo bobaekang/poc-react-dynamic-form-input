12dae720d031c5cc19d3b5b628e543e7656bb163
bobaekang <bkang.dev@gmail.com>
2020-07-08 10:19:40 -0500

    Display config and generated form side-by-side

--- src/App.tsx
+++ src/App.tsx
@@ -40,8 +40,19 @@ const initialValues = {
 
 function App() {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ margin: '0 auto' }}>
+    <div
+      style={{
+        fontFamily: 'sans-serif',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <div style={{ margin: '0 1rem' }}>
+        <h1>Config in JSON</h1>
+        <pre>{JSON.stringify(config, null, 2)}</pre>
+      </div>
+
+      <div style={{ margin: '0 1rem' }}>
         <h1>Generated form</h1>
         <Form config={config} initialValues={initialValues} />
       </div>
