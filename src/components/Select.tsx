0e5c094d3d60c7bb8530f46e2a766cbcc15e206e
Bobae Kang <bkang.dev@gmail.com>
2021-02-19 16:48:13 -0600

    Display select option description

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SelectOption } from '../model'
 import Label from './Label'
 
@@ -13,35 +13,56 @@ type SelectProps = {
   onChange?: React.ChangeEventHandler<HTMLSelectElement>
 }
 
+function getDescriptionMap(options: SelectOption[]) {
+  const descriptionMap: { [key: string]: string } = {}
+  for (const { value, description } of options)
+    descriptionMap[value] = description || ''
+
+  return descriptionMap
+}
+
 const Select = ({
   label,
   name,
   options,
   placeholder,
+  onChange,
   ...attr
-}: SelectProps) => (
-  <>
-    {label && <Label text={label} htmlFor={name || ''} />}
-    <select
-      {...attr}
-      id={name}
-      name={name}
-      style={{
-        minWidth: '200px',
-      }}
-    >
-      {placeholder && (
-        <option value="" hidden>
-          {placeholder}
-        </option>
-      )}
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label || option.value}
-        </option>
-      ))}
-    </select>
-  </>
-)
+}: SelectProps) => {
+  const [description, setDescription] = useState('')
+  const descriptionMap = getDescriptionMap(options)
+
+  return (
+    <>
+      {label && <Label text={label} htmlFor={name || ''} />}
+      <select
+        {...attr}
+        id={name}
+        name={name}
+        style={{
+          minWidth: '200px',
+        }}
+        onChange={(e) => {
+          setDescription(descriptionMap[e.target.value])
+          if (onChange) onChange(e)
+        }}
+      >
+        {placeholder && (
+          <option value="" hidden>
+            {placeholder}
+          </option>
+        )}
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label || option.value}
+          </option>
+        ))}
+      </select>
+      <div style={{ textAlign: 'center', marginTop: '.2em', fontSize: '.8em' }}>
+        {description && <em>{description}</em>}
+      </div>
+    </>
+  )
+}
 
 export default Select
